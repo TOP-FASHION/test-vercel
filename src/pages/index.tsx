ed01b1778c591e4bd343ac4c6b06b7f2d20ac20d
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-01-05 09:14:47 +0600

    deps: deps test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version perf
+        New version deps
         <div className={styles.center}>
           <Image
             className={styles.logo}
