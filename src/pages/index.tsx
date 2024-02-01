3041a01274ee558cc6b14585aa3b474d0d0571ea
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-02-01 15:33:20 +0600

    feat: check new version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        New version patch
+        New version main
         <div className={styles.center}>
           <Image
             className={styles.logo}
