150fa4f2a9caefa78f8cbac23e54ed7976d2152f
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-02-07 13:21:53 +0600

    feat!: add newrelic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import Appsignal from "@appsignal/javascript"
-
-new Appsignal({ key: "60967553-132c-49fd-ade4-319012762c85" })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,49 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.tsx
+const newrelic = require("newrelic");
+import Document, {
+    DocumentContext,
+    DocumentInitialProps,
+    Html,
+    Head,
+    Main,
+    NextScript,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        const browserTimingHeader = newrelic.getBrowserTimingHeader({
+            hasToRemoveScriptWrapper: true,
+        });
+
+
+        return {
+            ...initialProps,
+            // @ts-ignore
+            browserTimingHeader,
+        };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script
+                        type="text/javascript"
+                        // @ts-ignore
+                        dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
+                    />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
