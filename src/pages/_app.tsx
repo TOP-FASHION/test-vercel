639fb2dee50965a82648099c33b139389bbe15ca
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-02-07 09:50:52 +0600

    feat!: add zipyai

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import * as atatus from 'atatus-spa';
+import zipy from 'zipyai';
 
-atatus.config('6bd87624839d43bd9124a7d5a13a0725').install();
+zipy.init('71290866');
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
