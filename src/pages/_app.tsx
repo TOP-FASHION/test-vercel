a5656c18426b99805e1fd72c74b4f98999b1de60
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-02-07 09:03:17 +0600

    feat!: add atatus

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import * as atatus from 'atatus-spa';
+
+atatus.config('6bd87624839d43bd9124a7d5a13a0725').install();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
