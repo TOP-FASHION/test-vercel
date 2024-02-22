4892f14f586fa17281fcc410c73b6f32e2c0abea
Evgeniy Popov <evgeniy.popov@amatechs.com>
2024-02-22 10:27:22 +0600

    feat: new translations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({product}: any) {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div>Version: {BUILD_ID}</div>
-        Home page
+        Home page new new
         <>{t(`type`)}</>
         <Link href="/other">Other</Link>
         <div>
